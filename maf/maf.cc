ab8d53c380823c5c41103cad4edf17815442af6b
Frank Moreno <frankmoreno1993@gmail.com>
2014-11-23 13:07:55 -0500

    passing the length of the array by template

--- maf/maf.cc
+++ maf/maf.cc
@@ -1,23 +1,23 @@
 #include "maf.h"
 
 // Constructor
-maf::maf() : n(len), p(0), sum(0)
+template<class T, int len> maf<T,len>::maf() : n(len), p(0), sum(0)
 {
     // reserve the mem force 0.0 initial value
-    V = new T[n];
+    V = new T[len];
     for (int i = 0; i < n; i++) {
         V[i] = 0.0;
     }
 }
 
 // Returns the mean of the lastest "n" values
-T maf::mean() {
+template<class T,int len> T maf<T,len>::mean() {
     return sum / n;
 }
 
 // Inserts the new value and calculate
 // the sum of the lastest "n" values
-void maf::insert(T newvalue) {
+template<class T,int len> void maf<T,len>::insert(T newvalue) {
     V[p] = newvalue;
     sum += newvalue - V[(p+n-1) % n];
     p = (p+1) % n;
--- maf/maf.h
+++ maf/maf.h
@@ -1,18 +1,17 @@
 #ifndef github_com_maf_cpp_maf_h
 #define github_com_maf_cpp_maf_h
 
-namespace kfx {
-  template<type T> class maf {
-    public:
-      maf(int len);
-      T mean();
-      void insert(T newvalue);
-    private:
-      int n; // length
-      int p; // pivot
-      T * V; // Values array
-      T sum;
-  };
-}
+template<class T, int len> class maf {
+  public:
+    maf();
+    T mean();
+    void insert(T newvalue);
+  private:
+    int n; // length
+    int p; // pivot
+    T * V; // Values array
+    T sum;
+};
+
 
 #endif // github_com_maf_cpp_maf_h
